43673cde5fe45b44067044e26953c0f7ca17a7f8
Benkalio <officialtennysonbk@gmail.com>
2022-08-29 00:38:52 +0100

    set up the routes

--- Api/src/routes/officeRouter.js
+++ Api/src/routes/officeRouter.js
@@ -6,6 +6,48 @@ const officeRouter = express.Router();
 officeRouter.use(bodyParser.json('dev'));
 
 officeRouter.route('/')
-  .get()
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    next();
+  })
+  .get((req, res, next) => {
+    res.end('Sending you the recent government offices...');
+  })
+  .post((req, res, next) => {
+    res.end('Will add the office: ' + req.body.name +
+      ' with details: ' + req.body.description);
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put operation not supported on /offices');
+  })
+  .delete((req, res, next) => {
+    res.end('Deleting all the government offices!');
+  });
+
+officeRouter.route('/:officeId')
+  .all((req, res, next) => {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    next();
+  })
+  .get((req, res, next) => {
+    res.end('Sending government office with id: ' + req.body.params +
+      ' to you!');
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Cannot post office ' + req.params.dishId)
+  })
+  .put((req, res, params) => {
+    res.write('Updating: ' + req.params.officeId + '\n');
+    res.end('Posting office with id: ' +
+      req.body.name + 'with details: ' + req.body.description)
+  })
+  .delete((req, res, params) => {
+    res.end('Deleting offices with id: ' + req.params.officeId)
+  })
 
 modules.exports = officeRouter;
--- Api/src/routes/partyRouter.js
+++ Api/src/routes/partyRouter.js
@@ -5,8 +5,47 @@ import bodyParser from "body-parser";
 
 const partyRouter = express.Router();
 
-partyRouter.use("/", (req, res) => {
+partyRouter.use('/')
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    next();
+  })
+  .get((req, res, next) => {
+    res.end('Sending the registered political parties');
+  })
+  .post((req, res, next) => {
+    res.end('Adding Political party : ' + req.body.name + ' with details: ' + req.body.description);
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put operation not supported on this directory');
+  })
+  .delete((req, res, next) => {
+    res.end('Deleting all parties.');
+  });
 
-})
+partyRouter.route('/')
+  .all((req, res, next) => {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    next();
+  })
+  .get((req, res, next) => {
+    res.end('Sending political party with id: ' + req.body.params + ' to you!');
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Cannot post office ' + req.params.dishId)
+  })
+  .put((req, res, params) => {
+    res.write('Updating: ' + req.params.officeId + '\n');
+    res.end('Political party with id: ' +
+      req.body.name + 'with details: ' + req.body.description)
+  })
+  .delete((req, res, params) => {
+    res.end('Deleting political party with id: ' + req.params.officeId)
+  });
 
 modules.exports = partyRouter;
